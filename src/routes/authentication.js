a7cd182db3de5a81ff0532f1aae5d900d109e8f1
Samu Tamminen <samu.tamminen@columbiaroad.com>
2017-07-12 16:29:35 +0300

    authenticatio skeleton

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,14 +1,10 @@
 
-//app/auth/shopify-embedded?hmac=4a52a613d2b2363ef6cb19cd59eeb4bee11a9ff570566dc13c0ccf80642ada8e&shop=samusteststore.myshopify.com&timestamp=1499847378
 const Promise = require("bluebird");
 const util = require('util');
 const crypto = require('crypto');
-const winston = require('winston');
+const winston = require('winston'); // LOGGING
 const redis = require('../helpers/redisHelper');
-
-function saveNonce(shop, nonce) {
-  winston.info('shop ' + shop + ', nonce ' + nonce);
-}
+const unirest = require('unirest');
 
 module.exports = function(app) {
 
@@ -34,9 +30,9 @@ module.exports = function(app) {
           throw new Error();
         } else {
           const redirect_uri = `${process.env.BASE_URL}/auth/redirect/uri`;
-          const url = `https://${shop}.myshopify.com/admin/oauth/authorize` +
+          const url = `https://${shop}/admin/oauth/authorize` +
                       `?client_id=${process.env.SHOPIFY_API_KEY}&scope=${process.env.SHOPIFY_SCOPES}&` +
-                      `redirect_uri=${redirect_uri}&state=${nonce}&grant_options[]=per-user`;
+                      `redirect_uri=${redirect_uri}&state=${nonce}`;//&grant_options[]=per-user`;
 
           return res.redirect(url);
         }
@@ -44,8 +40,7 @@ module.exports = function(app) {
     });
   });
 
-  //https://example.org/some/redirect/uri?code={authorization_code}&hmac=da9d83c171400a41f8db91a950508985&timestamp=1409617544&state={nonce}&shop={hostname}
-  app.post('/auth/redirect/uri', function(req, res) {
+  app.get('/auth/redirect/uri', function(req, res) {
     req.checkQuery('code', 'Invalid or missing param').notEmpty();
     req.checkQuery('hmac', 'Invalid or missing param').notEmpty();
     req.checkQuery('timestamp', 'Invalid or missing param').notEmpty().isInt();
@@ -54,6 +49,32 @@ module.exports = function(app) {
 
     const {code, hmac, timestamp, state, shop} = req.query;
 
+    const url = `https://${shop}/admin/oauth/access_token`;
+    unirest.post(url)
+    .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+    .send({ "client_id": process.env.SHOPIFY_API_KEY,
+            "client_secret": process.env.SHOPIFY_APP_SECRET,
+            "code": code})
+    .end(function (response) {
+      if (response.status === 200) {
+        const access_token = response.body.access_token;
+        //SAVE access token and shop somewhere
+        // if we want more info about user, should change
+        // https://help.shopify.com/api/getting-started/authentication/oauth#api-access-modes
+        // to 'online access mode'
+        winston.info('ACCESS TOKEN ' + access_token);
+        return res.redirect(`${process.env.BASE_URL}/app_installed`);
+      } else {
+        return res.redirect(`${process.env.BASE_URL}/app_installation_failed`);
+      }
+    });
   });
 
+  app.get('/app_installed', function(req, res) {
+    res.send('Installation works! but no hmac checks done yet');
+  });
+
+  app.get('/app_installation_failed', function(req, res) {
+    res.send('Installation do not work :(');
+  });
 };
