a045b3b19e795000ee25ac5d5d132fb464ec7424
Max Reuter <max.a.reuter@gmail.com>
2017-07-13 14:56:35 +0300

    refactor to use official shopify library

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,47 +1,33 @@
-const axios = require('axios');
-const url = require('url');
-const apikey = process.env.API_KEY;
-const secret = process.env.API_SECRET;
-const shop = process.env.SHOP;
+const Shopify = require('shopify-api-node');
 
-const rootUrl = `https://${apikey}:${secret}@${shop}.myshopify.com/admin`;
+const apiKey = process.env.API_KEY;
+const password = process.env.API_SECRET;
+const shopName = process.env.SHOP;
 
-/**
- * Builds complete URL for Shopify's Admin API. Also
- * appends query parameters if existent.
- * 
- * @param {Request} req 
- * @param {string} path 
- */
-function buildUrl(req, path) {
-  // Append path to root URL
-  const baseUrl = `${rootUrl}${path}`
-  queryString = url.parse(req.url).query;
-  return queryString === null 
-      ? baseUrl
-      : `${baseUrl}?${queryString}`;
-}
+const shopify = new Shopify({
+  shopName: shopName,
+  apiKey: apiKey,
+  password: password
+});
 
 module.exports = (app) => {
   app.get('/api/v1/products', (req, res) => {
-    fullUrl = buildUrl(req, `/products.json`);
-    axios.get(fullUrl)
-      .then((result) => {
-        return res.json(result.data);
+    shopify.product.list(req.query)
+      .then((products) => {
+        return res.json(products);
       })
       .catch((err) => {
-        return res.json(err.response.data);
+        return res.json(err);
       });
   });
 
-  app.get('/api/v1/products/:productId', (req, res) => {
-    fullUrl = buildUrl(req, `/products/${req.params.productId}.json`);
-    axios.get(fullUrl)
-      .then((result) => {
-        return res.json(result.data);
+  app.get('/api/v1/products/:id', (req, res) => {
+    shopify.product.get(req.params.id, req.query)
+      .then((product) => {
+        return res.json(product);
       })
       .catch((err) => {
-        return res.json(err.response.data);
+        return res.json(err);
       })
   });
 }
