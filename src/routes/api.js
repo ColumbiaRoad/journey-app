6a25749c6c69dba1ca5bfc0e67b603c394f19acc
Max Reuter <max.a.reuter@gmail.com>
2017-07-13 13:12:10 +0300

    add query parameter support

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,23 +1,34 @@
 const axios = require('axios');
+const url = require('url');
 const apikey = process.env.API_KEY;
 const secret = process.env.API_SECRET;
 const shop = process.env.SHOP;
 
-const baseUrl = `https://${apikey}:${secret}@${shop}.myshopify.com/admin`;
+const rootUrl = `https://${apikey}:${secret}@${shop}.myshopify.com/admin`;
+
+function buildUrl(req, path) {
+  const baseUrl = `${rootUrl}${path}`
+  queryString = url.parse(req.url).query;
+  return queryString === null 
+      ? baseUrl
+      : `${baseUrl}?${queryString}`;
+}
 
 module.exports = (app) => {
   app.get('/api/v1/products', (req, res) => {
-    axios.get(`${baseUrl}/products.json`)
+    fullUrl = buildUrl(req, `/products.json`);
+    axios.get(fullUrl)
       .then((result) => {
         return res.json(result.data);
       })
       .catch((err) => {
-        return res.json(err);
+        return res.json(err.response.data);
       });
   });
 
   app.get('/api/v1/products/:productId', (req, res) => {
-    axios.get(`${baseUrl}/products/${req.params.productId}.json`)
+    fullUrl = buildUrl(req, `/products/${req.params.productId}.json`);
+    axios.get(fullUrl)
       .then((result) => {
         return res.json(result.data);
       })
