2534f89147b64ab6dc2517e5523ebad1004fcaf8
Samu Tamminen <samu.tamminen@columbiaroad.com>
2017-07-28 15:56:32 +0300

    return some liquid

--- src/routes/journey-assistant.js
+++ src/routes/journey-assistant.js
@@ -1,6 +1,9 @@
 
 module.exports = function(app) {
   app.get('/journey-assistant', function(req, res) {
-    return res.send('This should be either liquid or something that looks good to end user.');
+    const liquid = '<p> Your stores cart has {{ cart.item_count }} items. </p>';
+
+    res.setHeader('Content-Type:', 'application/liquid'); // Let's tell shopify that liquid is coming!
+    return res.send(liquid);
   });
 }
