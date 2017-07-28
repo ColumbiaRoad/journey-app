011c296d9afd331d1e3224f85d83203fb16fd8e8
Samu Tamminen <samu.tamminen@columbiaroad.com>
2017-07-28 16:02:39 +0300

    fix typo

--- src/routes/journey-assistant.js
+++ src/routes/journey-assistant.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.get('/journey-assistant', function(req, res) {
     const liquid = '<p> Your stores cart has {{ cart.item_count }} items. </p>';
 
-    res.setHeader('Content-Type:', 'application/liquid'); // Let's tell shopify that liquid is coming!
+    res.setHeader('content-type', 'application/liquid'); // Let's tell shopify that liquid is coming!
     return res.send(liquid);
   });
 }
