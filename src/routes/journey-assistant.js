18b3b28bfdb818a5a835cdca56e9848a298dcb54
Samu Tamminen <samu.tamminen@columbiaroad.com>
2017-07-31 09:04:46 +0300

    show mock question

--- src/routes/journey-assistant.js
+++ src/routes/journey-assistant.js
@@ -1,9 +1,33 @@
 
 module.exports = function(app) {
+
+  // REPLACE THIS BY DB CONNECTION
+  const getModel = (shop_url) => {
+    return [{
+      answer: 'big',
+      variant: {"random":"this object can be everything"},
+      question: 'big or small?',
+      shop_url:'salashoppi',
+      access_token:'access_token'
+    }];
+  };
+
+  const formatOneQuestion = (question_row) => {
+    return '<div>' +
+            '<p>' + question_row.question + '</p>' +
+            '<p>' + question_row.answer +
+           '</div>';
+  };
+
   app.get('/journey-assistant', function(req, res) {
-    const liquid = '<p> Your stores cart has {{ cart.item_count }} items. </p>';
+    const shop_url = req.body.shop_url;
+    const model = getModel(shop_url);
+    let liquidHTML = '';
+    model.rows.forEach((question_row) => {
+      liquidHTML += formatOneQuestion(question_row);
+    });
 
     res.setHeader('content-type', 'application/liquid'); // Let's tell shopify that liquid is coming!
-    return res.send(liquid);
+    return res.send(liquidHTML);
   });
 }
