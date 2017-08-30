14c3f96b434f20f7c5af47aeacba020dc1726d70
Max Reuter <max.a.reuter@gmail.com>
2017-08-30 10:29:33 +0300

    group shopify helpers together

--- src/helpers/shopifyHelper.js
+++ src/helpers/shopifyHelper.js
@@ -1,38 +1,40 @@
-
+const ShopifyToken = require('shopify-token');
 const Shopify = require('shopify-api-node');
 const shopModel = require('../models/shop');
 const winston = require('winston'); // LOGGING
 
-let shopify = undefined;
-
 const getShopifyInstance = (shop) => {
   return new Promise((resolve, reject) => {
-    if(shopify === undefined) {
     shopModel.getShop(shop)
-      .then((shop) => {
-        if(shop !== undefined) {
-          winston.info(`shop: ${shop.shop_url}`);
-          shopify = new Shopify({
-            shopName: shop.shop_url.split('.')[0],
-            accessToken: shop.access_token,
-            autoLimit: true
-          });
-          resolve(shopify);
-        } else {
-          reject({
-            message: 'Unkown shop'
-          });
-        }
-      })
-      .catch((err) => {
-        reject(err);
-      });
-    } else {
-      resolve(shopify);
-    }
+    .then((shop) => {
+      if(shop === null) {
+        shopify = new Shopify({
+          shopName: shop.shop_url.split('.')[0],
+          accessToken: shop.access_token,
+          autoLimit: true
+        });
+        resolve(shopify);
+      } else {
+        reject({
+          message: 'Unkown shop'
+        });
+      }
+    })
+    .catch((err) => {
+      winston.error(err);
+      reject(err);
+    });
   });
 }
 
+const getShopifyToken = () => {
+  return new ShopifyToken({
+    sharedSecret: process.env.SHOPIFY_APP_SECRET,
+    redirectUri: `${process.env.BASE_URL}/auth/install`,
+    apiKey: process.env.SHOPIFY_API_KEY
+  });
+};
+
 module.exports = {
-  getShopifyInstance
+  getShopifyInstance, getShopifyToken
 }
