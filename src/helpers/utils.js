d7123a75a9f313ffe7da394f92eeabfa8f05e303
Max Reuter <max.a.reuter@gmail.com>
2017-09-01 11:02:23 +0300

    add scopes to tokens

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -5,17 +5,10 @@ const validationError = (result) => {
   return 'There have been validation errors: ' + util.inspect(result.array());
 };
 
-//https://stackoverflow.com/questions/14446511/what-is-the-most-efficient-method-to-groupby-on-a-javascript-array-of-objects
-const groupBy = (xs, key) => {
-  return xs.reduce(function(rv, x) {
-    (rv[x[key]] = rv[x[key]] || []).push(x);
-    return rv;
-  }, {});
-};
-
-const getJWTToken = (shop) => {
+const getJWTToken = (shop, scope) => {
   const payload = {
-    shop: shop
+    shop: shop,
+    scope: scope
   };
   return jwt.sign(payload, process.env.SHOPIFY_APP_SECRET, { expiresIn: '3h' });
 }
@@ -24,6 +17,11 @@ const decodeJWTToken = (token) => {
   return jwt.verify(token, process.env.SHOPIFY_APP_SECRET);
 }
 
+const scopes = {
+  api: 'api',
+  app: 'application-proxy'
+}
+
 module.exports = {
-  validationError, groupBy, getJWTToken, decodeJWTToken
+  validationError, getJWTToken, decodeJWTToken, scopes
 };
