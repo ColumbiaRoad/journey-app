1f9a00b724634807b6054ce00d333b9d94bc3858
Max Reuter <max.a.reuter@gmail.com>
2017-09-01 11:03:34 +0300

    check scope right when accessing api routes

--- src/config/express.js
+++ src/config/express.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const jwt = require('express-jwt');
+const scopes = require('../helpers/utils').scopes;
 
 const allowCrossDomain = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', process.env.ACCESS_CONTROL_ALLOW_ORIGIN);
@@ -31,11 +32,21 @@ module.exports = function() {
     secret: process.env.SHOPIFY_APP_SECRET,
     requestProperty: 'auth'
   };
-  // Require JWT token for all paths but the ones starting with /auth/
+
+  const guard = require('express-jwt-permissions')({
+    requestProperty: 'auth',
+    permissionsProperty: 'scope'
+  });
+
+  // Require JWT token for all paths but the ones starting with /auth/ or /journey-assistant
   app.use(jwt(jwtConfig).unless({path: [/^\/auth\//, /^\/journey-assistant/]}));
+  // Require API right to access api
+  app.use(/^\/api\//, guard.check(scopes.api));
   app.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
       res.status(401).json('Invalid token');
+    } else if (err.code === 'permission_denied') {
+      res.status(401).send('Insufficient Permissions');
     }
   });
 
