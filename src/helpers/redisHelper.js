156c6ec037b65816c249efd91f528302c6e1b599
Max Reuter <max.a.reuter@gmail.com>
2017-09-07 12:11:32 +0300

    increase expiration time to 1 day

--- src/helpers/redisHelper.js
+++ src/helpers/redisHelper.js
@@ -22,7 +22,8 @@ function getNonceByShop(shop, callback) {
 function setNonceByShop(shop, nonce, callback) {
   const client = getClient();
   client.on('error', (err) => callback(err));
-  client.set(shop, nonce, 'EX', 300, () => {
+  // Key expires after 1 day
+  client.set(shop, nonce, 'EX', 86400, () => {
     client.quit();
     callback(null);
   });
